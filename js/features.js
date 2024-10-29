c56ae3faa7f5e7289ee1e82737f43df193586b36
FaysalMiah55 <faysalmiah369@gmail.com>
2024-10-30 01:28:47 +0600

    donation and toggle bg color change

--- js/features.js
+++ js/features.js
@@ -1,11 +1,18 @@
 // donate form
 document.getElementById("btn-donation")
     .addEventListener('click', function(){
+        console.log("show btn clicked");
         showSectionById("donation-form")
+        showButtonById("btn-donation")
     })
 
 // history form
 document.getElementById("btn-history")
     .addEventListener('click', function(){
+        console.log("history clicked");
         showSectionById("history-form")
+        showButtonById("btn-history")
+
+        
     })
+
--- js/utilites.js
+++ js/utilites.js
@@ -14,9 +14,18 @@ function getTextFieldValueById(id){
 
 // toggle feature section
 function showSectionById(id){
-    // hidden all the section
-    document.getElementById("donation-form").classList.add("hidden")
-    document.getElementById("history-form").classList.add("hidden")
-    // show the section
-    document.getElementById(id).remove("hidden")
+    // hide all the section
+    document.getElementById("donation-form").classList.add('hidden')
+    document.getElementById("history-form").classList.add('hidden')
+    
+    // show the section with provide id as parameter
+    document.getElementById(id).classList.remove('hidden')
+    
+}
+function showButtonById(button){
+    // hide all the background color
+    document.getElementById("btn-donation").classList.remove('bg-[#B4F461]')
+    document.getElementById("btn-history").classList.remove('bg-[#B4F461]')
+    // add background to the active button
+    document.getElementById(button).classList.add('bg-[#B4F461]')
 }
