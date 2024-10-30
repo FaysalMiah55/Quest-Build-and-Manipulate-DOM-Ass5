f9bc07738f0c6ebc07461b6080c6fa7bbfa8e223
FaysalMiah55 <faysalmiah369@gmail.com>
2024-10-30 10:31:01 +0600

    blog and home

--- js/features.js
+++ js/features.js
@@ -1,7 +1,6 @@
 // donate form
 document.getElementById("btn-donation")
     .addEventListener('click', function(){
-        console.log("show btn clicked");
         showSectionById("donation-form")
         showButtonById("btn-donation")
     })
@@ -9,7 +8,6 @@ document.getElementById("btn-donation")
 // history form
 document.getElementById("btn-history")
     .addEventListener('click', function(){
-        console.log("history clicked");
         showSectionById("history-form")
         showButtonById("btn-history")
     })
---
+++ js/home.js
@@ -0,0 +1,5 @@
+document.getElementById('home')
+    .addEventListener('click', function(){
+        console.log('home clicked');
+        window.location.href = '/index.html'
+    })
